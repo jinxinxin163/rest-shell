a0b4dce5c011760275fd59b66eed9d319c6ee644
root <jin.xin@advantech.com.cn>
2020-03-19 09:54:10 +0800

    some minor modify

--- buildenv.sh
+++ buildenv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export GOPATH=/app/web/gopath/
 export GO111MODULE=on
 export GOPROXY=https://mirrors.aliyun.com/goproxy/
--- newProject.sh
+++ newProject.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-workdir=rest-shell
-appname=restshell
-servicename=ShellService
+workdir=Log-MeterAgent
+appname=meteragent
+servicename=LogMeterAgent
+lowservicename=logmeteragent
 sed -i  "s/MP_buy_API/$workdir/g" `grep MP_buy_API -rl ./`
+sed -i  "s/mpbuyservice/$lowservicename/g" `grep mpbuyservice -rl ./`
 sed -i  "s/mpbuy/$appname/g" `grep mpbuy -rl ./`
 sed -i  "s/MpBuyService/$servicename/g" `grep MpBuyService -rl ./`
+sed -i  "s/MpbuyService/$servicename/g" `grep MpbuyService -rl ./`
 
 mv pkg/api/mpbuy pkg/api/$appname
 mv pkg/apiserver/mpbuy pkg/apiserver/$appname
 mv pkg/apis/mpbuy pkg/apis/$appname
-mv cmd/mpbuyservice cmd/${appname}service
+mv cmd/mpbuyservice cmd/${lowservicename}
+mv package/mpbuyservice package/${lowservicename}
+mv package/mpbuyservice/deploy/mpbuyservice.yaml package/${lowservicename}/deploy/${lowservicename}.yaml 
